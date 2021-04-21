1bc69d6e86f8ec0cd398bff9a7138a6446c4109c
nixigg86 <dev.py86@gmail.com>
2021-04-20 22:05:18 -0700

    Finished with react and started with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Proshop</h1> 
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
